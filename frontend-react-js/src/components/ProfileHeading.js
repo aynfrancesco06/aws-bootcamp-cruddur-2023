813afbc966d722a72bbcf7a73b128eb8a73658a0
humayn <aynfrancesco@gmail.com>
2023-04-21 06:10:31 +0000

    Render Avatar from cloudfront.

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,5 +1,6 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
+import ProfileAvatar from './ProfileAvatar';
 
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.thecloudproject.store/avatars/data.jpg")';
@@ -13,12 +14,11 @@ export default function ProfileHeading(props) {
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles} >
-      <div className="avatar">
-        <img src="https://assets.thecloudproject.store/avatars/data.jpg"></img>
-      </div>
+      <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>
+      {props.profile.display_name}
         <div className="display_name">{props.profile.display_name}</div>
         <div className="handle">@{props.profile.handle}</div>
       </div>
